d0bffd09c13238e66143df42b399c50d471903df
Ahmad Mazaal <ahmad.hmazaal@gmail.com>
2020-03-02 22:45:30 +0200

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const forecast=require("./utils/forecast");
 const geocode=require("./utils/geocode");
 
 const app= express();
-
+const port = process.env.PORT || 3000
 // Define paths for express config
 const publicDirectory= path.join(__dirname,'../public');
 const viewsPath= path.join(__dirname,'../templates/views')
@@ -94,7 +94,7 @@ app.get('*',(req,res)=>{
 //     res.send([{features:{Long:21,Lat:22},center:{name:"ahmad",age:24}}]);
 // });
 
-app.listen(3000,()=>{
-    console.log("server is up on port 3000")
+app.listen(port,()=>{
+    console.log("server is up on port " + port)
     
 });
